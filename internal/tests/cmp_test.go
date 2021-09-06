de574f60a5e0acb61b7a672905c86af2a880892d
Michael McLoughlin <mmcloughlin@gmail.com>
2021-09-05 17:04:39 -0700

    internal/tests: diff with openssl

---
+++ internal/tests/cmp_test.go
@@ -0,0 +1,35 @@
+package tests
+
+import (
+	"bytes"
+	"hash"
+	"math/rand"
+	"testing"
+
+	"github.com/mmcloughlin/md4"
+	openssl "github.com/ricardobranco777/go-openssl/md4"
+)
+
+func TestLengths(t *testing.T) {
+	N := 1 << 14
+	buf := make([]byte, N)
+	for n := 0; n < N; n++ {
+		// Initialize random buffer.
+		b := buf[:n]
+		rand.Read(b)
+
+		// Expected hash.
+		expect := sum(openssl.New(), b)
+		got := sum(md4.New(), b)
+
+		if !bytes.Equal(expect, got) {
+			t.FailNow()
+		}
+	}
+}
+
+func sum(h hash.Hash, b []byte) []byte {
+	h.Reset()
+	h.Write(b)
+	return h.Sum(nil)
+}
